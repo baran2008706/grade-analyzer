f78722b1e9be28a7fd5d732285f3731351c6940d
baran2008706 <barantashakori0@gmail.com>
2026-09-23 20:46:24 +0330

    Add files via upload

---
+++ GradeAnalyzer.py
@@ -0,0 +1,13 @@
+courses = {}
+num = int(input("enter the number of courses:"))
+
+total = 0
+for i in range(1, num + 1):
+    course = input("enter course name:")
+    #we use float here because the scores can be any number in the range of real numbers
+    scores = float(input("enter your score:"))
+    courses[course] = scores
+    total += scores
+
+print("your average is:", total/num)
+print(courses)
